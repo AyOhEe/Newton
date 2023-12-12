5729828209f76421c93cae1babc961b933bb0bb5
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-12-12 19:54:07 +0000

    Added manual crouching
    Player can now crouch using the right stick. Realised I need to do snap/smooth turn too, so that's on the list with sprinting in terms of body core functionality

--- Scripts/LegsJoint.cs
+++ Scripts/LegsJoint.cs
@@ -10,15 +10,22 @@ public partial class LegsJoint : Generic6DofJoint3D
 	[Export] private CollisionShape3D _LocosphereCollision;
 
     [ExportCategory("Jumping settings")]
-    [Export] private float _CrouchingEquilibrium;
-    [Export] private float _CrouchSpeed = 3;
     [Export] private float _JumpCrouchSpeed = 1;
+
+    [ExportCategory("Crouching settings")]
+    [Export] private float _CrouchSpeed = 3;
+    [Export] private float _CrouchingDeadzone = 0.1f;
+    [Export] private float _MinCrouchEquilibrium = 0.2f;
+    [Export] private float _MaxCrouchEquilibrium = 0.9f;
 
 
     private float _WorkingCrouchSpeed;
     private float _CurrentEquilibrium;
     private bool _PreparingJump;
     private bool _ReleasedJump;
+
+
+    private Vector2 _CrouchThumbstickInput;
 
 
     public override void _Ready()
@@ -31,16 +38,27 @@ public partial class LegsJoint : Generic6DofJoint3D
     public override void _PhysicsProcess(double delta)
 	{
         float targetEquilibrium = RequiredLegExtension();
-        if (_PreparingJump)
-        {
-            targetEquilibrium = _CrouchingEquilibrium;
-        }
-        else if (_ReleasedJump)
+        //if the player just stopped preparing to jump, spring the legs back to full length
+        if (_ReleasedJump)
         {
             _CurrentEquilibrium = targetEquilibrium;
             _ReleasedJump = false;
         }
+        //if the player wants to jump, override the target equilibrium
+        else if (_PreparingJump)
+        {
+            targetEquilibrium = _MinCrouchEquilibrium;
+        }
+        
 
+        //if the player wants to crouch, override the target equilibrium
+        if (PlayerCrouchInputActive(targetEquilibrium, out float desiredEquilibrium))
+        {
+            targetEquilibrium = desiredEquilibrium;
+        }
+
+
+        //approach the target equilibrium over time
         _CurrentEquilibrium = (float)Mathf.MoveToward(_CurrentEquilibrium, targetEquilibrium, delta * _WorkingCrouchSpeed);
         SetParamY(Param.LinearSpringEquilibriumPoint, _CurrentEquilibrium);
     }
@@ -55,6 +73,30 @@ public partial class LegsJoint : Generic6DofJoint3D
         //distance between locosphere center and chest center
         float requiredExtension = chestHeight - locosphereRadius - chestMountOffset;
         return requiredExtension;
+    }
+
+    private bool PlayerCrouchInputActive(float targetEquilibrium, out float desiredEquilibrium)
+    {
+        float absInput = Mathf.Abs(_CrouchThumbstickInput.Y);
+        if (absInput <= _CrouchingDeadzone)
+        {
+            desiredEquilibrium = 0;
+            return false;
+        }
+
+        //https://www.desmos.com/calculator/qxdvl74fdm
+        float lerp = (absInput - _CrouchingDeadzone) / (1 - _CrouchingDeadzone);
+        if (_CrouchThumbstickInput.Y < 0)
+        {
+            desiredEquilibrium = Mathf.Lerp(targetEquilibrium, _MinCrouchEquilibrium, lerp);
+            desiredEquilibrium = Mathf.Min(targetEquilibrium, desiredEquilibrium);
+        }
+        else
+        {
+            desiredEquilibrium = Mathf.Lerp(targetEquilibrium, _MaxCrouchEquilibrium, lerp);
+            desiredEquilibrium = Mathf.Max(targetEquilibrium, desiredEquilibrium);
+        }
+        return true;
     }
 
     public void ControllerButtonPressed(string name)
@@ -73,6 +115,14 @@ public partial class LegsJoint : Generic6DofJoint3D
             _WorkingCrouchSpeed = _CrouchSpeed;
             SetJumpingStatus(false);
             _ReleasedJump = true;
+        }
+    }
+
+    public void ControllerVec2Changed(string name, Vector2 value)
+    {
+        if (name == "primary")
+        {
+            _CrouchThumbstickInput = value;
         }
     }
 
