78e926edfeb256356ef1f7f9e8f975cc42760a17
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-12-22 14:12:16 +0000

    Turning now occurs around the player camera
    It used to occur around the camera rig origin, this was not intended and seemed to be an issue with the meat-space movement matching

--- Scripts/MoveWithChest.cs
+++ Scripts/MoveWithChest.cs
@@ -15,9 +15,14 @@ public partial class MoveWithChest : Node
 	public override void _PhysicsProcess(double delta)
 	{
 		//get the distance the chest moved on the XZ plane this tick
-		Vector3 chestPos = _CameraRig.GlobalBasis * _BodySolver.GetChestPos();
+		//the chest position is first calculated relative to the camera rig,
+		//only taking into account scaling
+		Vector3 chestPos = Scale(_BodySolver.GetChestPos(), _CameraRig.GlobalBasis.Scale);
 		Vector3 chestDelta = chestPos - _LastChestPos;
 		chestDelta.Y = 0;
+        //once the difference has been obtained, it is then rotated to match the camera rig
+        //if we do not do this, turning occurs around the camera rig origin instead of around the camera
+        chestDelta = _CameraRig.GlobalBasis.Orthonormalized() * chestDelta;
 
 
 		//move all rig rigidbodies by this change
@@ -29,5 +34,12 @@ public partial class MoveWithChest : Node
 
 		//store the chest position for the next tick
         _LastChestPos = chestPos;
+	}
+
+	private Vector3 Scale(Vector3 A, Vector3 B)
+	{
+		A.Deconstruct(out float ax, out float ay, out float az);
+		B.Deconstruct(out float bx, out float by, out float bz);
+		return new Vector3(ax * bx, ay * by, az * bz);
 	}
 }
