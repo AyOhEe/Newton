0c778af3389aecc99922136052e5fe5fdf9735d8
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-05-23 18:09:10 +0100

    Improved grabs
    Seems like the hands were overshooting, and thus anything the hands *held* was overshooting. No more wrong physics!

--- Scripts/Grabbables/TwoHandedGrabbableJoint.cs
+++ Scripts/Grabbables/TwoHandedGrabbableJoint.cs
@@ -120,18 +120,15 @@ public partial class TwoHandedGrabbableJoint : Node
         Vector3 angularVelocity = totalInertia.Inverse() * angularMomentum;
 
         //the bodies will have the same angular velocity as they are joined together perfectly
-        _GrabbableRB.AngularVelocity = angularVelocity * 0.95f;
-        _LeftHandRB.AngularVelocity = angularVelocity * 0.95f;
-        _RightHandRB.AngularVelocity = angularVelocity * 0.95f;
+        _GrabbableRB.AngularVelocity = angularVelocity;
+        _LeftHandRB.AngularVelocity = angularVelocity;
+        _RightHandRB.AngularVelocity = angularVelocity;
 
 
         //from COM velocity and angular velocity, we can calculate the linear velocity
-        _LeftHandRB.LinearVelocity = (COMVel + (lHandCOM - COM).Cross(angularVelocity)) * 0.95f;
-        _RightHandRB.LinearVelocity = (COMVel + (rHandCOM - COM).Cross(angularVelocity)) * 0.95f;
-        _GrabbableRB.LinearVelocity = (COMVel + (grabbableCOM - COM).Cross(angularVelocity)) * 0.95f;
-
-        //TODO: I hate this fix. *0.95f everything to prevent overshooting is a shit solution.
-        //      The algo shouldn't overshoot in the first place. This is incorrect physics.
+        _LeftHandRB.LinearVelocity = (COMVel + (lHandCOM - COM).Cross(angularVelocity));
+        _RightHandRB.LinearVelocity = (COMVel + (rHandCOM - COM).Cross(angularVelocity));
+        _GrabbableRB.LinearVelocity = (COMVel + (grabbableCOM - COM).Cross(angularVelocity));
     }
 
     public static Basis AddBases(Basis left, Basis right)
